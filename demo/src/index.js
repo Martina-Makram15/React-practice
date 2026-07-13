267f637b8332a4f88936b8000cb235f8abe4d334
Martina Makram <martina.makram@dafagate.com>
2026-07-13 09:16:31 +0300

    feat: init the project with react and add product component

---
+++ demo/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Product from "./product";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<Product />);
